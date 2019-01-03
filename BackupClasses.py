e197d05c349d59079aea2e9769f00b4c8a68da1e
JPJ-WM <jack.pj@live.co.uk>
2019-01-03 14:52:15 +0000

    Expanded filediff check

--- BackupClasses.py
+++ BackupClasses.py
@@ -4,15 +4,32 @@ class HomeBackup:
     def __init__(self, livedir, backupdir):
         """For working with the live/backup folders"""
         self.livedir = livedir
-        self. backupdir = backupdir
+        self.backupdir = backupdir
 
     def filediff(self):
         """Will print two lists of files. 
         Missing from backup, and removed from live"""
+
+
+
+
         for dir_, _, files in os.walk(self.livedir):
             for file_name in files:
-                print('=====')
+                print('==========================================')
+                # Get live path info
+                full_dir = os.path.join(dir_, file_name)    # Full path
+                print(f'Full Directory : {full_dir}')
+
                 rel_dir = os.path.relpath(dir_, self.livedir)
-                print(f'rel_dir : {rel_dir}')
-                rel_file = os.path.join(rel_dir, file_name)
-                print(f'rel_file : {rel_file}')
+                rel_file = os.path.join(rel_dir, file_name) # Relative path
+                print(f'Relative Path  : {rel_file}') 
+
+                # Generate backup path
+                back_dir = os.path.join(self.backupdir, file_name)
+                print(f'Back Directory : {back_dir}')
+
+                # check if file is backed up
+                if os.path.isfile(back_dir):
+                    print('Status         : Backed up')
+                else:
+                    print('Status         : Not backed up')
