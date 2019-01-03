de6359391e0536b62a25994d7dd171b7f65c925a
JPJ-WM <jack.pj@live.co.uk>
2019-01-03 15:14:12 +0000

    Added filesize check

--- BackupClasses.py
+++ BackupClasses.py
@@ -31,5 +31,23 @@ class Home_Backup:
                 # check if file is backed up
                 if os.path.isfile(back_dir):    # Check if same name file exists
                     print('Status         : Backed up')
+                    check_if_duplicate(full_dir, back_dir)
                 else:
                     print('Status         : Not backed up')
+
+
+def check_if_duplicate(path_1, path_2):
+    """Checks if two files are a duplicate"""
+    # First check both sizes
+    size_1 = 0
+    size_2 = 2
+    try:
+        size_1 = os.path.getsize(path_1)
+        size_2 = os.path.getsize(path_2)
+        print(f'Size 1 : {size_1}   -   Size 2 : {size_2}')
+    except (OSError,):
+        # not accessible (permissions, etc) - pass on
+        pass
+    if size_1 != size_2:    # If sizes are different, not a duplicate
+        return False
+    
